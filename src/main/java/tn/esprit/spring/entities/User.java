37735c9692a8cac69ff68c3e4ea88390cc810d4a
Naoufel <naoufelmhadhbi2@gmail.com>
2021-12-11 16:47:23 +0100

    User modified

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 	
-	
+	//commentaire 1
 	private static final long serialVersionUID = 1L;
 
 	@Id
