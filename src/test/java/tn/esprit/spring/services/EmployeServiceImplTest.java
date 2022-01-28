d7261b490687da29c749bd71d1caacb3418d7e8f
Idriss Mezguich <idriss.mezguich@esprit.tn>
2022-01-28 22:22:49 +0100

    fix ids

--- src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
@@ -25,21 +25,18 @@ public class EmployeServiceImplTest {
 	IEmployeService es;
 	
 	@Test
-	@Order (2)
+	@Order (1)
 	public void testretrieveAllEmployees() {		
 		List<Employe> listEmployees = es.retrieveEmployees();
-		//ma base de données contient quatre users
-		// je vais vérifier si ma base me retourne vraiment trois users avec la methode assertEquals 
-		Assertions.assertEquals(1, listEmployees.size());
+		Assertions.assertEquals(0, listEmployees.size());
 	}
 	
 	@Test
-	@Order (1)
+	@Order (2)
 	public void testaddEmploye() throws ParseException, java.text.ParseException {
 			
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		//Date d = dateFormat.parse("2015-03-12");
-		Employe e = new Employe("Ahmed", "Ben Ahmed", "ahmed.benahmed@test.com", true, Role.INGENIEUR); // le user ça sera ajouté avec l'id 4 puisque j'ai trois ancien users
+		Employe e = new Employe("Ahmed", "Ben Ahmed", "ahmed.benahmed@test.com", true, Role.INGENIEUR);
 		Employe EmployeAdded = es.addEmploye(e);
 		Assertions.assertEquals(e.getNom(), EmployeAdded.getNom());
 			
@@ -50,7 +47,7 @@ public class EmployeServiceImplTest {
 	public void testupdateEmploye() throws ParseException, java.text.ParseException {
 			
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Employe e = new Employe(1L, "Ahmed_modified", "Ben Ahmed", "ahmed.benahmed@test.com", true, Role.INGENIEUR); // L pour dire de type long | D double | F float
+		Employe e = new Employe(1L, "Ahmed_modified", "Ben Ahmed", "ahmed.benahmed@test.com", true, Role.INGENIEUR);
 		Employe employeUpdated = es.updateEmploye(e);
 		Assertions.assertEquals(e.getNom(), employeUpdated.getNom());
 			
@@ -60,7 +57,7 @@ public class EmployeServiceImplTest {
 	@Order (4)
 	public void testretrieveEmploye() {
 		
-		Employe EmployeRetrieved = es.getEmploye(Long.parseLong("4")); 
+		Employe EmployeRetrieved = es.getEmploye(Long.parseLong("1")); 
 		Assertions.assertEquals(1L, EmployeRetrieved.getId());
 		
 	}
@@ -69,8 +66,8 @@ public class EmployeServiceImplTest {
 	@Order (5)
 	public void testdeleteEmploye() {
 		
-		es.deleteEmploye(Long.parseLong("4")); 
-		Assertions.assertNull(es.getEmploye(Long.parseLong("4")));
+		es.deleteEmploye(Long.parseLong("1")); 
+		Assertions.assertNull(es.getEmploye(Long.parseLong("1")));
 		
 	}
 }
